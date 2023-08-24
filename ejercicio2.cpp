f9ee2eb29bbd59daf2061d967c7e53e38f816e21
stefanoaragoni <stefanoaragoni13@gmail.com>
2023-08-24 10:09:51 -0600

    cambio 4

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -91,10 +91,10 @@ int main() {
 
     // Leer los números en un arreglo
     int* readNumbers = new int[N];
-    #pragma omp parallel for    // CAMBIO 3: Paralelizar el bucle
+    //#pragma omp parallel for    // CAMBIO 3: Paralelizar el bucle
     for (int i = 0; i < N; ++i) {
         char comma;
-        #pragma omp critical    // Critical section para evitar que se lea del archivo al mismo tiempo
+        //#pragma omp critical    // Critical section para evitar que se lea del archivo al mismo tiempo
         inFile >> readNumbers[i] >> comma;
     }
     inFile.close();
@@ -104,9 +104,9 @@ int main() {
     
     // Escribir los números ordenados en otro archivo
     std::ofstream sortedFile("sorted_numbers_P.csv");
-    //#pragma omp parallel for    // CAMBIO 4: Paralelizar el bucle
+    #pragma omp parallel for    // CAMBIO 4: Paralelizar el bucle
     for (int i = 0; i < N; ++i) {
-        //#pragma omp critical    // Critical section para evitar que se escriba en el archivo al mismo tiempo
+        #pragma omp critical    // Critical section para evitar que se escriba en el archivo al mismo tiempo
         sortedFile << readNumbers[i];
         if (i < N - 1) {
             sortedFile << ",";
